914aa2e86435f6420cec8d8020f1d5d29a069aff
EnesOzdmir <159448492+EnesOzdmir@users.noreply.github.com>
2025-12-24 08:34:16 +0300

    Update main.c

--- gazi_birinci_proje/main.c
+++ gazi_birinci_proje/main.c
@@ -1,17 +1,17 @@
-// Include'lar
+// Includelar
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 //Constlar
+const char AIDAT_ADRESI[] = "/home/enes/Masaüstü/aidat.txt";
+const char TOPLAM_ADRESI[] = "/home/enes/Masaüstü/toplam.txt";
+const char ISLEM_SAYISI = 15;
+//Hepsi Farklıysa Önlem Olması için doğru yazın.
+//Proje 15 sınırı olduğundan çok önem verilmemiştir
 
-//Adresler
-const char AIDAT_ADRESI[] = "C:\\Users\\Cihan\\OneDrive\\Desktop\\aidat.txt";
-const char TOPLAM_ADRESI[] = "C:\\Users\\Cihan\\OneDrive\\Desktop\\toplam.txt";
 
-const char ISLEM_SAYISI = 15; //Hepsi Farklıysa Önlem Olması için doğru yazın. Proje 15 sınırı olduğundan çok önem verilmemiştir
-
-// Her bir daire sakini için isim, soyisim ve aidat bilgisini tutan yapı
+//Her bir daire sakini için isim, soyisim ve aidat bilgisini tutan yapı
 struct daire {
     char ad[128];
     char soyad[128];
@@ -32,8 +32,6 @@ struct daire {
  * yazdir fonksiyonu temel olarak 'daire' tipindeki diziyi düzenler ve
  * 'TOPLAM_ADRESI' diye tanımlanan yere çıktısını yazar.
  */
-
-//Fonksiyonları Çağır
 void ekle(struct daire* kayitlar, int* kullanici_sayisi);
 void kesme(struct daire *isleme, char veri[128]);
 void yazdir(struct daire bilgiler[], int kullanici_sayisi);
@@ -45,17 +43,16 @@ int main(void) {
 
     //Ekle fonksiyonunu çağır
     ekle(kayitlar, &kullanici_sayisi);
-    
+
     //Yıllık aidatları toplam.txt dosyasına yazdır
     yazdir(kayitlar, kullanici_sayisi);
 
     return 0;
 }
 
-//Ekle Fonksiyonu
 void ekle(struct daire* kayitlar,int* kullanici_sayisi) {
     char satir_verisi[128]; //Tüm satırları okusun diye konuldu
-    struct daire gecici_kayit; //Veri structin kopyasini olustur
+    struct daire gecici_kayit; //Struct Daire türünde geçici veri depolar olustur
 
     //Dosyayı kontrol et
     FILE *dosya = fopen(AIDAT_ADRESI, "r");
@@ -71,9 +68,9 @@ void ekle(struct daire* kayitlar,int* kullanici_sayisi) {
     while (fgets(satir_verisi, sizeof(satir_verisi), dosya) != NULL) {
         kesme(&gecici_kayit, satir_verisi);
 
-        // Proje ödevi dahilinde 15 istek sınırlandırılması için yapılmıştır.
+        // Proje ödevi dahilinde 'ISLEM_SAYISI'[Bize verilen görevde 15 kişi] istek sınırlandırılması için yapılmıştır.
         for (int i = 0; i < ISLEM_SAYISI+1; i++) {
-            // Yine proje ödevi yüzünden max 15 kişi olabilceğinden Index 15(Kişi sayisi - 1) olamacağından 16 da otomatik kişi eklemesi için yapılmıştır.
+            // Yine proje ödevi yüzünden max 'ISLEM_SAYISI' kişi olabilceğinden Index kişi sayısıyla(Kişi sayisi - 1) aynı olamacağından 'ISLEM_SAYISI'da otomatik kişi eklemesi için yapılmıştır.
             if (i == ISLEM_SAYISI) {
                 kayitlar[(*kullanici_sayisi)] = gecici_kayit;
                 *kullanici_sayisi += 1;
@@ -92,8 +89,8 @@ void ekle(struct daire* kayitlar,int* kullanici_sayisi) {
 
 void kesme(struct daire *isleme, char veri[128]) {
 
-    // struchIndex: en son kaçıncı tokeni aldığımızı anlamamızı sağlar
-    int struchIndex = 0;
+    // tokenIndex: en son kaçıncı tokeni aldığımızı anlamamızı sağlar
+    int tokenIndex = 0;
     // sinir: en son ki karakterin kaçıncı Indexi tutar
     int sinir       = 0;
 
@@ -102,30 +99,30 @@ void kesme(struct daire *isleme, char veri[128]) {
      * Gelen satırdaki(veri) onun 'i'ninci karekteri 'null' veya 'enter' olana kadar 'i'ninci karekteri artırıp devam etmesini sağlar
      */
     for (int i = 0; veri[i] != '\0' && veri[i] != '\n'; i++) {
-        if (veri[i] == ' ' && struchIndex == 0) {
+        if (veri[i] == ' ' && tokenIndex == 0) {
             strncpy(isleme->ad, veri, i);
             isleme->ad[i-sinir] = '\0';
             sinir = i;
-            struchIndex = 1;
+            tokenIndex = 1;
         }
 
-        else if (veri[i] == ' ' && struchIndex == 1) {
+        else if (veri[i] == ' ' && tokenIndex == 1) {
             strncpy(isleme->soyad, veri + sinir +1, i-sinir-1);
             isleme->soyad[i-sinir-1] = '\0';
 
 
             sinir = i;
-            struchIndex = 2;
+            tokenIndex = 2;
         }
-        else if (struchIndex == 2 && veri[i+1] == '\n') {
+        else if (tokenIndex == 2 && veri[i+1] == '\n') {
             char geciciDeger[32];
             strncpy(geciciDeger, veri + sinir + 1 , i-sinir);
             isleme->aidat = strtof(geciciDeger, NULL);
             sinir = i;
 
-            struchIndex = 3;
+            tokenIndex = 3;
         }
-        else if (struchIndex == 3) {
+        else if (tokenIndex == 3) {
             break;
         }
     }
@@ -141,9 +138,8 @@ void yazdir(struct daire bilgiler[], int kullanici_sayisi) {
         return;
     }
 
-    /*
-     * Her birini tek tek yazma işlemi.
-     */
+
+    //Her bir satırı tek tek yazma işlemi.
     for (int i = 0; i < kullanici_sayisi; i++) {
         fprintf(dosya, "%s %s %.2f\n",
                 bilgiler[i].ad,
